9db7af7714840fe7315d51b9c88c0cf39dc86ca3
ayechan <ayechan.sant@gmail.com>
2024-11-14 12:05:06 -0800

    init

---
+++ planets/target/debug/deps/planets-cc027286748f7453.d
@@ -0,0 +1,5 @@
+/Users/ayechan/projects/star-wars-planets/planets/target/debug/deps/planets-cc027286748f7453: src/main.rs
+
+/Users/ayechan/projects/star-wars-planets/planets/target/debug/deps/planets-cc027286748f7453.d: src/main.rs
+
+src/main.rs:
---
+++ planets/target/debug/planets.d
@@ -0,0 +1,1 @@
+/Users/ayechan/projects/star-wars-planets/planets/target/debug/planets: /Users/ayechan/projects/star-wars-planets/planets/src/main.rs
